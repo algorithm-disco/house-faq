a3b8d3527cc56225030106993a1cdc962e47217c
stanleylsx <gzlishouxian@gmail.com>
2020-10-31 19:07:45 +0800

    训练预测逻辑

--- engines/train.py
+++ engines/train.py
@@ -8,20 +8,56 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from engines.data import DataPrecessForSentence
 from engines.bert_esim_model import BertEsimModel
+from engines.utils.metrics import correct_predictions, cal_metrics
 from sklearn.model_selection import GroupKFold
 from transformers.optimization import AdamW
 from tqdm import tqdm
 import torch
+import time
+
+
+def evaluate(logger, device, model, criterion, dev_data_loader):
+    """
+    验证集评估函数，分别计算每个类别的f1、precision、recall
+    """
+    model.eval()
+    start_time = time.time()
+    loss_sum = 0.0
+    correct_preds = 0
+    all_predicts = []
+    all_labels = []
+    with torch.no_grad():
+        for step, (batch_ids, batch_masks, batch_segments, batch_labels) in enumerate(tqdm(dev_data_loader)):
+            ids, masks, segments, labels = batch_ids.to(device), batch_masks.to(device), batch_segments.to(
+                device), batch_labels.to(device)
+            logits, probabilities = model(ids, masks, segments)
+            loss = criterion(logits, labels)
+            loss_sum += loss.item()
+            correct_preds += correct_predictions(probabilities, labels)
+            predicts = torch.argmax(probabilities, dim=1)
+            all_predicts.extend(predicts.cpu())
+            all_labels.extend(batch_labels.cpu())
+    val_time = time.time() - start_time
+    val_loss = loss_sum / len(dev_data_loader)
+    val_accuracy = correct_preds / len(dev_data_loader.dataset)
+    val_measures = cal_metrics(all_predicts, all_labels)
+    val_measures['accuracy'] = val_accuracy
+    res_str = ''
+    for k, v in val_measures.items():
+        res_str += (k + ': %.3f ' % v)
+    logger.info('training step: %5d, loss: %.5f, %s' % (step, val_loss, res_str))
+    logger.info('time consumption of evaluating:%.2f(min)' % val_time)
+    return val_measures
 
 
 def train(device, logger):
     batch_size = 128
     epoch = 30
-    learning_rate = 2e-05
-    adam_epsilon = 1e-05
+    learning_rate = 0.0004
     patience = 3
-    train_query_file = 'datasets/train/train.query.1.tsv'
-    train_reply_file = 'datasets/train/train.reply.1.tsv'
+    print_per_batch = 40
+    train_query_file = 'datasets/train/train.query.tsv'
+    train_reply_file = 'datasets/train/train.reply.tsv'
     # 加载训练语料
     train_left = pd.read_csv(train_query_file, sep='\t', header=None)
     train_left.columns = ['id', 'query']
@@ -30,9 +66,7 @@ def train(device, logger):
     train_data = train_left.merge(train_right, how='left')
     train_data['reply'] = train_data['reply'].fillna('好的')
 
-    model = BertEsimModel(device).to(device)
-    params = list(model.parameters())
-    optimizer = AdamW(params, lr=learning_rate, eps=adam_epsilon)
+    criterion = torch.nn.CrossEntropyLoss()
 
     # N折交叉验证
     gkf = GroupKFold(n_splits=5).split(X=train_data.reply, groups=train_data.id)
@@ -42,16 +76,49 @@ def train(device, logger):
         logger.info('train_data_length:{}\n'.format(len(train_data_manger)))
         train_loader = DataLoader(train_data_manger, shuffle=True, batch_size=batch_size)
 
-        dev_data_manger = DataPrecessForSentence(train_data.iloc[valid_idx], logger)
-        logger.info('dev_data_length:{}\n'.format(len(dev_data_manger)))
-        dev_loader = DataLoader(dev_data_manger, shuffle=True, batch_size=batch_size)
+        val_data_manger = DataPrecessForSentence(train_data.iloc[valid_idx], logger)
+        logger.info('dev_data_length:{}\n'.format(len(val_data_manger)))
+        val_loader = DataLoader(val_data_manger, shuffle=True, batch_size=batch_size)
 
-    # for i in range(epoch):
-    #     logger.info('epoch:{}/{}'.format(i + 1, epoch))
-    #     for step, (batch_ids, batch_masks, batch_segments, batch_labels) in enumerate(tqdm(train_loader)):
-    #         ids, masks, segments, labels = batch_ids.to(device), batch_masks.to(device), batch_segments.to(
-    #             device), batch_labels.to(device)
-    #         logits, probabilities = model(ids, masks, segments)
+        model = BertEsimModel(device).to(device)
+        params = list(model.parameters())
+        optimizer = AdamW(params, lr=learning_rate)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="max", factor=0.5, patience=0)
+        for i in range(epoch):
+            train_start = time.time()
+            logger.info('epoch:{}/{}'.format(i + 1, epoch))
+            loss, loss_sum = 0.0, 0.0
+            correct_preds = 0
+            model.train()
+            for step, (batch_ids, batch_masks, batch_segments, batch_labels) in enumerate(tqdm(train_loader)):
+                ids, masks, segments, labels = batch_ids.to(device), batch_masks.to(device), batch_segments.to(
+                    device), batch_labels.to(device)
+                optimizer.zero_grad()
+                logits, probabilities = model(ids, masks, segments)
+                loss = criterion(logits, labels)
+                loss.backward()
+                optimizer.step()
+                loss_sum += loss.item()
+                correct_preds += correct_predictions(probabilities, labels)
+                # 打印下训练过程中的指标
+                if step % print_per_batch == 0 and step != 0:
+                    predicts = torch.argmax(probabilities, dim=1)
+                    measures = cal_metrics(predicts.cpu(), labels.cpu())
+                    res_str = ''
+                    for k, v in measures.items():
+                        res_str += (k + ': %.3f ' % v)
+                    logger.info('training step: %5d, loss: %.5f, %s' % (step, loss, res_str))
+            train_time = (time.time() - train_start) / 60
+            train_accuracy = correct_preds / len(train_loader.dataset)
+            scheduler.step(train_accuracy)
+            logger.info('time consumption of training:%.2f(min)' % train_time)
+            logger.info('start evaluate engines...')
+            val_measures = evaluate(logger, device, model, criterion, val_loader)
+
+
+
+
+
 
 
 
--- engines/utils/metrics.py
+++ engines/utils/metrics.py
@@ -4,3 +4,23 @@
 # @Email : gzlishouxian@gmail.com
 # @File : metrics.py
 # @Software: PyCharm
+from sklearn import metrics
+import numpy as np
+
+
+def cal_metrics(predicts, targets):
+    """
+    指标计算
+    """
+    average = 'binary'
+    precision = metrics.precision_score(predicts, targets, average=average)
+    recall = metrics.recall_score(predicts, targets, average=average)
+    f1 = metrics.f1_score(predicts, targets, average=average)
+    return {'precision': precision, 'recall': recall, 'f1': f1}
+
+
+def correct_predictions(output_probabilities, targets):
+    _, out_classes = output_probabilities.max(dim=1)
+    correct = out_classes == targets
+    correct_nums = correct.sum()
+    return correct_nums.item()
